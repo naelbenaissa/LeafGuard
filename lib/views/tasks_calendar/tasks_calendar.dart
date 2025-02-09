f7dc7663827a9865861751bb763ddca975e1a270
naelb <naelbenaissapro@gmail.com>
2025-02-08 21:45:34 -0500

    Ajustement routes

--- lib/views/tasks_calendar/tasks_calendar.dart
+++ lib/views/tasks_calendar/tasks_calendar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-
 import '../bar/custom_appbar.dart';
 import '../bar/custom_bottombar.dart';
 
