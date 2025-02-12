7cb185be46328b4b66b73c0b8b485ee510c44cd4
naelb <naelbenaissapro@gmail.com>
2025-02-11 22:28:39 -0500

    Ajustement profileButton

--- lib/views/bar/widgets/profileButton.dart
+++ lib/views/bar/widgets/profileButton.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 import '../../../services/user_service.dart';
 
 class ProfileButton extends StatefulWidget {
