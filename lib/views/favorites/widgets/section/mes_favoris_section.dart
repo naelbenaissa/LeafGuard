4501fbf8478a58f123b8055264d7360dc27943cb
naelb <naelbenaissapro@gmail.com>
2025-02-25 17:32:05 -0500

    Ajout scroll favorite

--- lib/views/favorites/widgets/section/mes_favoris_section.dart
+++ lib/views/favorites/widgets/section/mes_favoris_section.dart
@@ -28,9 +28,7 @@ class _MesFavorisSectionState extends State<MesFavorisSection> {
 
   /// Récupère les favoris de l'utilisateur avec les détails des plantes
   Future<void> _fetchFavorites() async {
-    setState(() {
-      isLoading = true;
-    });
+    setState(() => isLoading = true);
 
     final user = supabase.auth.currentUser;
     if (user != null) {
@@ -38,9 +36,8 @@ class _MesFavorisSectionState extends State<MesFavorisSection> {
       final plantIds = await favoriteService.getFavoritePlantIds(userId!);
 
       List<Map<String, dynamic>> plants = [];
-
-      final futures = plantIds
-          .map((plantId) => trefleApiService.fetchPlantDetails(plantId));
+      final futures =
+      plantIds.map((plantId) => trefleApiService.fetchPlantDetails(plantId));
       final results = await Future.wait(futures);
 
       for (int i = 0; i < results.length; i++) {
@@ -68,49 +65,65 @@ class _MesFavorisSectionState extends State<MesFavorisSection> {
     }
 
     return favoritePlants.isEmpty
-        ? const Center(
-            child: Text("Pas de favoris", style: TextStyle(fontSize: 18)))
-        : ListView.builder(
-            itemCount: favoritePlants.length,
-            padding: const EdgeInsets.all(10),
-            itemBuilder: (context, index) {
-              final plant = favoritePlants[index];
-              return Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+        ? RefreshIndicator(
+      onRefresh: _fetchFavorites,
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.7, // Pour activer le scroll même vide
+          child: const Center(
+            child: Text("Pas de favoris", style: TextStyle(fontSize: 18)),
+          ),
+        ),
+      ),
+    )
+        : RefreshIndicator(
+      onRefresh: _fetchFavorites,
+      child: ListView.builder(
+        physics: const AlwaysScrollableScrollPhysics(), // Permet le scroll fluide
+        itemCount: favoritePlants.length + 1, // +1 pour le SizedBox final
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (context, index) {
+          if (index == favoritePlants.length) {
+            return const SizedBox(height: 50); // Ajoute un espace à la fin
+          }
+
+          final plant = favoritePlants[index];
+          return Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            child: ListTile(
+              contentPadding: const EdgeInsets.all(10),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  plant['plant_image'],
+                  width: 60,
+                  height: 60,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) =>
+                  const Icon(Icons.local_florist, size: 50),
                 ),
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                child: ListTile(
-                  contentPadding: const EdgeInsets.all(10),
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      plant['plant_image'],
-                      width: 60,
-                      height: 60,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) =>
-                          const Icon(Icons.local_florist, size: 50),
-                    ),
-                  ),
-                  title: Text(
-                    plant['plant_name'],
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text("${plant['plant_id']}"),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.favorite, color: Colors.red),
-                    onPressed: () async {
-                      await favoriteService.removeFavorite(
-                          userId!, plant['plant_id']);
-                      _fetchFavorites();
-                    },
-                  ),
-                ),
-              );
-            },
+              ),
+              title: Text(
+                plant['plant_name'],
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("${plant['plant_id']}"),
+              trailing: IconButton(
+                icon: const Icon(Icons.favorite, color: Colors.red),
+                onPressed: () async {
+                  await favoriteService.removeFavorite(userId!, plant['plant_id']);
+                  _fetchFavorites();
+                },
+              ),
+            ),
           );
+        },
+      ),
+    );
   }
 }
--- lib/views/favorites/widgets/section/mes_scans_sections.dart
+++ lib/views/favorites/widgets/section/mes_scans_sections.dart
@@ -70,51 +70,61 @@ class _MesScansSectionState extends State<MesScansSection> {
     }
 
     return scans.isEmpty
-        ? const Center(
-            child:
-                Text("Aucun scan disponible", style: TextStyle(fontSize: 18)))
+        ? RefreshIndicator(
+      onRefresh: _fetchScans,
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.7,
+          child: const Center(
+            child: Text("Aucun scan disponible", style: TextStyle(fontSize: 18)),
+          ),
+        ),
+      ),
+    )
         : RefreshIndicator(
-            onRefresh: _fetchScans,
-            child: ListView.builder(
-              itemCount: scans.length,
-              padding: const EdgeInsets.all(10),
-              itemBuilder: (context, index) {
-                final scan = scans[index];
-                return Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.all(10),
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        scan['image_url'],
-                        width: 60,
-                        height: 60,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) =>
-                            const Icon(Icons.image, size: 50),
-                      ),
-                    ),
-                    title: Text(
-                      scan['predictions'],
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                        "Confiance: ${(scan['confidence'] * 100).toStringAsFixed(1)}%"),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.close, color: Colors.red),
-                      onPressed: () =>
-                          _confirmDelete(scan['id'], scan['image_url']),
-                    ),
-                  ),
-                );
-              },
+      onRefresh: _fetchScans,
+      child: ListView.builder(
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemCount: scans.length + 1,
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (context, index) {
+          if (index == scans.length) {
+            return const SizedBox(height: 50);
+          }
+
+          final scan = scans[index];
+          return Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            child: ListTile(
+              contentPadding: const EdgeInsets.all(10),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  scan['image_url'],
+                  width: 60,
+                  height: 60,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) =>
+                  const Icon(Icons.image, size: 50),
+                ),
+              ),
+              title: Text(
+                scan['predictions'],
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  "Confiance: ${(scan['confidence'] * 100).toStringAsFixed(1)}%"),
+              trailing: IconButton(
+                icon: const Icon(Icons.close, color: Colors.red),
+                onPressed: () => _confirmDelete(scan['id'], scan['image_url']),
+              ),
             ),
           );
+        },
+      ),
+    );
   }
 }
