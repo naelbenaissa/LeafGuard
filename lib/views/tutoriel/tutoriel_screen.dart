07ac288aacfd1712fb89519321a5c2fb47718651
naelb <naelbenaissapro@gmail.com>
2025-02-27 12:39:04 -0500

    Tutoriel de début fait

--- lib/views/tutoriel/tutoriel_screen.dart
+++ lib/views/tutoriel/tutoriel_screen.dart
@@ -10,9 +10,8 @@ class OnboardingScreen extends StatelessWidget {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('onboarding_complete', true);
 
-    // Utiliser `Future.microtask` pour éviter des conflits avec le contexte de navigation
     Future.microtask(() {
-      context.go('/'); // Aller à la page d'accueil après onboarding
+      context.go('/');
     });
   }
 
@@ -20,36 +19,43 @@ class OnboardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return IntroductionScreen(
       pages: [
-        PageViewModel(
+        _buildPage(
           title: "Bienvenue sur LeafGuard",
-          body: "Scannez, comprenez et agissez pour prendre soin de vos plantes !",
-          image: Image.asset("assets/img/onboarding1.png", height: 250),
-          decoration: _pageDecoration(),
+          body:
+              "Scannez, comprenez et agissez pour prendre soin de vos plantes !",
+          imagePath: "assets/img/tutoriel/home.png",
         ),
-        PageViewModel(
+        _buildPage(
           title: "Ajoutez des tâches",
-          body: "Planifiez facilement l’entretien de vos plantes avec le calendrier intégré.",
-          image: Image.asset("assets/img/onboarding2.png", height: 250),
-          decoration: _pageDecoration(),
+          body:
+              "Planifiez facilement l’entretien de vos plantes avec le calendrier intégré.",
+          imagePath: "assets/img/tutoriel/task.png",
         ),
-        PageViewModel(
+        _buildPage(
           title: "Diagnostic intelligent",
-          body: "Utilisez l’IA pour identifier les maladies de vos plantes et recevoir des conseils.",
-          image: Image.asset("assets/img/onboarding3.png", height: 250),
-          decoration: _pageDecoration(),
+          body:
+              "Utilisez l’IA pour identifier les maladies de vos plantes et recevoir des conseils.",
+          imagePath: "assets/img/tutoriel/diagnostic.png",
         ),
-        PageViewModel(
+        _buildPage(
           title: "Découvrez plus de 300 000 plantes",
-          body: "Accédez à un guide complet avec des informations détaillées sur chaque plante.",
-          image: Image.asset("assets/img/onboarding4.png", height: 250),
-          decoration: _pageDecoration(),
+          body:
+              "Accédez à un guide complet avec des informations détaillées sur chaque plante.",
+          imagePath: "assets/img/tutoriel/guide.png",
         ),
       ],
       onDone: () => _onIntroEnd(context),
       showSkipButton: true,
-      skip: const Text("Passer"),
-      next: const Icon(Icons.arrow_forward),
-      done: const Text("Terminé", style: TextStyle(fontWeight: FontWeight.bold)),
+      skip: const Text(
+        "Passer",
+        style: TextStyle(color: Colors.black),
+      ),
+      next: const Icon(
+        Icons.arrow_forward,
+        color: Colors.black,
+      ),
+      done: const Text("Terminé",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
       dotsDecorator: DotsDecorator(
         size: const Size(10.0, 10.0),
         color: Colors.grey,
@@ -62,12 +68,31 @@ class OnboardingScreen extends StatelessWidget {
     );
   }
 
-  PageDecoration _pageDecoration() {
-    return const PageDecoration(
-      titleTextStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-      bodyTextStyle: TextStyle(fontSize: 16.0),
-      imagePadding: EdgeInsets.all(20),
-      pageColor: Colors.white,
+  PageViewModel _buildPage({
+    required String title,
+    required String body,
+    required String imagePath,
+  }) {
+    return PageViewModel(
+      // title: title,
+      titleWidget: const SizedBox(),
+      bodyWidget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(imagePath),
+          const SizedBox(height: 40),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            body,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
     );
   }
 }
